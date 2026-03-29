6788d4ca54a641662bb72a6806615e8797b3dd24
Rajat Jakhar <76389609+jakhar434@users.noreply.github.com>
2026-03-29 10:50:04 +0530

    Remove router - always render Home directly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,15 @@
-import { Switch, Route } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch hook={useHashLocation}>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Home />
       </TooltipProvider>
     </QueryClientProvider>
   );
